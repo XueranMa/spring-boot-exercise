7a91673899a348d86ab66e363901dedd5798860e
Xueran Ma <xma@mulesoft.com>
2021-09-17 20:19:21 -0700

    [INIT] Init the project with Maven and Spring Boot (spring web)

---
+++ src/main/java/com/xma/springbootproduction/SpringbootProductionApplication.java
@@ -0,0 +1,13 @@
+package com.xma.springbootproduction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootProductionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootProductionApplication.class, args);
+    }
+
+}
