bfc8afa6dea343b727e4574a62e8c084b844cea3
Xueran Ma <xma@mulesoft.com>
2021-09-21 15:56:40 -0700

    [ADD] Added unit test cases for get and post. Fixed the package errors.

--- src/test/java/com/xma/springbootproduction/UnitTests.java
+++ src/test/java/com/xma/springbootproduction/UnitTests.java
@@ -1,19 +1,25 @@
 package com.xma.springbootproduction;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.xma.springbootproduction.controllers.Controller;
-import org.hamcrest.Matchers;
+
+import com.xma.springbootproduction.models.Message;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
-import org.junit.Assert;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.json.BasicJsonTester;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 
 
 // Test Standalone Controller
@@ -29,15 +35,32 @@ public class UnitTests {
     MockMvc mockMvc;
 
     @Test
-    public void testGet() throws Exception {
-
-        mockMvc.perform( MockMvcRequestBuilders
-                .get("/message")
-                .accept(MediaType.APPLICATION_JSON))
-                .andDo(print()))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$", Matchers.hasSize(1)))
-                .andExpect(jsonPath("$[0]", Matchers.is("first message")))
+    public void testGetMsg() throws Exception {
+        mockMvc.perform(get("/api/message")).andDo(print()).andExpect(status().isOk())
+                .andExpect(content().string(containsString("first message")));
     }
 
+    @Test
+    public void testGetSay() throws Exception {
+        mockMvc.perform(get("/api/helloworld")).andDo(print()).andExpect(status().isOk())
+                .andExpect(content().string(containsString("Hello World")));
+    }
+
+    @Test
+    public void testPost() throws Exception {
+        mockMvc.perform(post("/api/message")
+                        .content(asJsonString(new Message("post test msg")))
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString("post test msg")));
+    }
+
+    public static String asJsonString(final Object obj) {
+        try {
+            return new ObjectMapper().writeValueAsString(obj);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
